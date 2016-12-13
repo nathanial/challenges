1599f6ad51c605843717511fc1b64856a4bd3bd0
Nathan Hartman <nathanial.hartman@gmail.com>
2016-12-13 05:22:50 -0800

    it persists

--- challenge-290-blinking-leds/src/components/App.jsx
+++ challenge-290-blinking-leds/src/components/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import _ from 'lodash';
-import {observable} from 'mobx';
+import {observable, autorun, reaction} from 'mobx';
 import {observer} from 'mobx-react';
 import CodeMirror from 'react-codemirror';
 
@@ -16,7 +16,7 @@ function LED(props){
 	};
 
 	if(props.on){
-		style.background = 'green';
+		style.background = '#98FB98';
 	} else {
 		style.background = 'white';
 	}
@@ -27,17 +27,49 @@ function LED(props){
 	);
 }
 
+class Machine {
+
+	@observable leds = [];
+	@observable code = '';
+
+	constructor(){
+		const json = JSON.parse(localStorage.getItem('machine') || '{}');
+		_.defaults(json, {
+			leds: [
+				{id: 0, on: false},
+				{id: 1, on: false},
+				{id: 2, on: false},
+				{id: 3, on: false},
+				{id: 4, on: false},
+				{id: 5, on: false},
+				{id: 6, on: false},
+				{id: 7, on: false}
+			],
+			code: ''
+		});
+
+		this.leds = observable(json.leds);
+		this.code = json.code;
+	}
+
+	toggleLED = (id) => {
+		const led = _.find(this.leds, {id});
+		led.on = !led.on;
+		this.persist();
+	}
+
+	updateCode = (newCode) => {
+		this.code = newCode;
+		this.persist();
+	}
+
+	persist = () => {
+		localStorage.setItem('machine', JSON.stringify(this));
+	}
+}
+
 export class AppState {
-	leds = observable([
-		{id: 0, on: false},
-		{id: 1, on: false},
-		{id: 2, on: false},
-		{id: 3, on: false},
-		{id: 4, on: false},
-		{id: 5, on: false},
-		{id: 6, on: false},
-		{id: 7, on: false}
-	]);
+	machine = new Machine();
 }
 
 @observer
@@ -50,22 +82,42 @@ export class App extends React.Component {
 		const editorOptions = {
 			lineNumbers: true
 		};
+		const editorStyle = {
+			border: '1px solid black'
+		};
+		const ledPanelStyle = {
+			marginTop: 10,
+			marginBottom: 20,
+			border: '1px solid black',
+			display: 'inline-block'
+		};
 		const appState = this.props.appState;
 		return (
 			<div>
-				{this._renderLEDs()}
-				<CodeMirror value={appState.code} onChange={this._updateCode} options={editorOptions} />
+				<div style={ledPanelStyle}>
+					{this._renderLEDs()}
+				</div>
+				<div style={editorStyle}>
+					<CodeMirror value={appState.machine.code}
+											onChange={this._updateCode}
+											options={editorOptions} />
+				</div>
 			</div>
 		);
 	}
 
 	_renderLEDs = () => {
-		return _.map(this.props.appState.leds, (led, i) => {
-			return <LED key={i} on={led.on} onClick={() => this._onToggleLED(led)} />
+		const machine = this.props.appState.machine;
+		return _.map(machine.leds, (led, i) => {
+			return <LED key={i} on={led.on} onClick={() => this._onToggleLED(led.id)} />
 		});
 	}
 
-	_onToggleLED = (toggledLED) => {
-		toggledLED.on = !toggledLED.on;
+	_onToggleLED = (id) => {
+		this.props.appState.machine.toggleLED(id);
+	}
+
+	_updateCode = (newCode) => {
+		this.props.appState.machine.updateCode(newCode);
 	}
 }
