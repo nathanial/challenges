01c50db879cb9c28ee79952770ac938d0b4ca35e
Nathan Hartman <nathanial.hartman@gmail.com>
2016-12-13 03:48:09 -0800

    basic electron app

---
+++ challenge-290-blinking-leds/src/app.js
@@ -0,0 +1,10 @@
+require('node-jsx').install();
+const React = require('react');
+const ReactDOM = require('react-dom');
+const {App} = require('./components/App.jsx');
+
+function main(){
+	ReactDOM.render(React.createElement(App), document.getElementById('app'));
+}
+main();
+
---
+++ challenge-290-blinking-leds/src/components/App.jsx
@@ -0,0 +1,13 @@
+const React = require('react');
+
+class App extends React.Component {
+	render(){
+		return (
+			<h1>Woot</h1>
+		);
+	}
+}
+
+module.exports = {
+	App
+};
